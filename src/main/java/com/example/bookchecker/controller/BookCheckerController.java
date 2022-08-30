dac3ef8128e55ed9f651b49f5751cb171cbdd6de
maxfnine <maxfnine@gmail.com>
2022-08-30 21:49:54 +0300

    Added books status table

--- src/main/java/com/example/bookchecker/controller/BookCheckerController.java
+++ src/main/java/com/example/bookchecker/controller/BookCheckerController.java
@@ -1,19 +1,28 @@
 package com.example.bookchecker.controller;
 
-import com.example.bookchecker.model.dto.BrandModelOutput;
 import com.example.bookchecker.service.BooksServiceJSONFile;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.IOException;
-import java.net.http.HttpResponse;
-import java.util.List;
 
 @Controller
 public class BookCheckerController {
+    private final BooksServiceJSONFile booksService;
+
+    public BookCheckerController(BooksServiceJSONFile booksService) {
+        this.booksService = booksService;
+    }
+
     @GetMapping("/")
     public String getMainPage() throws IOException {
         return "index";
+    }
+
+    @GetMapping("/showstatus")
+    public String showStatus(Model model){
+        model.addAttribute("brands",booksService.checkBooks());
+        return "showStatus";
     }
 }
