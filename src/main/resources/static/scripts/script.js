25cdf69a4dcb62cc941a057ca41a843d82cb654b
maxfnine <maxfnine@gmail.com>
2022-12-28 22:15:31 +0200

    Added Chery(FX,T7,T8)

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -2,6 +2,7 @@ const renaultUrl = 'https://cloud.info.renault.co.il/catalog?m=';
 const daciaUrl = 'https://cloud.info.dacia.co.il/catalog?m=';
 const nissanUrl = 'https://cloud.info.lease4u.co.il/platenumber?m=';
 const infinitiUrl = 'https://cloud.info.infiniti-cars.co.il/catalog?m=';
+const cheryUrl = 'https://cloud.info.cheryisrael.co.il/catalog?m=';
 const renaultInput = document.getElementById('renault-input');
 const renaultLink = document.getElementById('renault-link');
 
@@ -14,10 +15,14 @@ const nissanLink = document.getElementById('nissan-link');
 const infinitiInput = document.getElementById('infiniti-input');
 const infinitiLink = document.getElementById('infiniti-link');
 
+const cheryInput = document.getElementById('chery-input');
+const cheryLink = document.getElementById('chery-link');
+
 renaultInput.addEventListener('input', (e) => renaultLink.setAttribute('href', `${renaultUrl}${e.target.value}`));
 daciaInput.addEventListener('input', (e) => daciaLink.setAttribute('href', `${daciaUrl}${e.target.value}`));
 nissanInput.addEventListener('input', (e) => nissanLink.setAttribute('href', `${nissanUrl}${e.target.value}`));
 infinitiInput.addEventListener('input', (e) => infinitiLink.setAttribute('href', `${infinitiUrl}${e.target.value}`));
+cheryInput.addEventListener('input', (e) => cheryLink.setAttribute('href', `${cheryUrl}${e.target.value}`));
 
 renaultLink.addEventListener('click', (e) => {
     e.preventDefault();
@@ -54,6 +59,16 @@ infinitiLink.addEventListener('click', (e) => {
     if (infinitiInput.value.length >= 8) {
         window.open(infinitiLink.getAttribute('href', '_blank'));
         infinitiInput.value = '';
+    } else {
+        alert('Invalid number length');
+    }
+});
+
+cheryLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (cheryInput.value.length >= 8) {
+        window.open(cheryLink.getAttribute('href', '_blank'));
+        cheryInput.value = '';
     } else {
         alert('Invalid number length');
     }
